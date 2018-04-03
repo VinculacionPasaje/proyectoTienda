25fb3d3b28aef8050678c89c1c9bffcb04bdaa6f
Fernando C <grupoturismopasaje@gmail.com>
2018-04-02 23:21:23 -0500

    cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
-@section('content')
-<div class="container">
+@section('contenido')
+
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+         <div class="col-md-3 ">
+
+
+         </div>
+        <div class="col-md-6">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading" style="font-size:  30px;text-align: center;padding-bottom: 15px;">Login</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
@@ -14,8 +18,8 @@
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                            <div class="col-md-6" style="max-width: 100%;">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus style="background-color: #f0f0f0;">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -28,8 +32,8 @@
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                            <div class="col-md-6" style="max-width: 100%;">
+                                <input id="password" type="password" class="form-control" name="password" required style="background-color: #f0f0f0;">
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -64,6 +68,13 @@
                 </div>
             </div>
         </div>
+
+         <div class="col-md-3">
+
+
+         </div>
+
+
     </div>
-</div>
+
 @endsection
